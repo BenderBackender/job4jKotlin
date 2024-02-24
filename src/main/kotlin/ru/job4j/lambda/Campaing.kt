c9bd50ebfbff56c7f586db375a2567b80986520f
Artyom <tema.ru2011@gmail.com>
2024-02-24 16:54:04 +0300

    Lambda calc stream Done

--- src/main/kotlin/ru/job4j/lambda/Campaing.kt
+++ src/main/kotlin/ru/job4j/lambda/Campaing.kt
@@ -5,17 +5,16 @@ import java.util.*
 
 fun main() {
     val list = mutableListOf<Campaing>()
-    list.add(Campaing("Mercedes", Address("Berlin","Pushkina","10"),Date()))
-    list.add(Campaing("BMW", Address("Bawaria","Lenina","101"),Date()))
+    list.add(Campaing("Mercedes", Address("Berlin", "Pushkina", "10"), Date()))
+    list.add(Campaing("BMW", Address("Bawaria", "Lenina", "101"), Date()))
     val rsl = getStringDescription(list)
     println(rsl.get(0))
     println(rsl.get(1))
 
 }
 
-fun getStringDescription(list: List<Campaing>): List<String> {
-    return list.map({ el -> "Company name : ${el.name} Addres: ${el.address} Date create : ${el.created}"})
-}
+fun getStringDescription(list: List<Campaing>) =
+    list.map({ el -> "Company name : ${el.name} Addres: ${el.address} Date create : ${el.created}" })
 
 data class Campaing(val name: String, val address: Address, val created: Date)
 
--- src/main/kotlin/ru/job4j/lambda/LambdaCalc.kt
+++ src/main/kotlin/ru/job4j/lambda/LambdaCalc.kt
@@ -1,8 +1,18 @@
 package ru.job4j.lambda
 
+import java.util.*
+import java.util.stream.Collectors
+
 fun main() {
-    val dec = {value:Int -> value-1}
-    val squaring = {value:Int -> value*value }
+    val dec = { value: Int -> value - 1 }
+    val squaring = { value: Int -> value * value }
     println(dec(10))
     println(squaring(10))
+    val list = listOf<Int>(2,8,20)
+    println(count(list))
+
+}
+
+fun count(list: List<Int>): Int {
+    return list.stream().filter { it > 10 }.map { it + 1 }.collect(Collectors.summingInt() { it })
 }
