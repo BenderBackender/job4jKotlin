d4b1e5083516127553b0995c7bb7f4e58a84556e
Artyom <tema.ru2011@gmail.com>
2024-02-24 16:07:26 +0300

    Lambda Map Function for Campaing Class Done

---
+++ src/main/kotlin/ru/job4j/lambda/Campaing.kt
@@ -0,0 +1,22 @@
+package ru.job4j.lambda
+
+
+import java.util.*
+
+fun main() {
+    val list = mutableListOf<Campaing>()
+    list.add(Campaing("Mercedes", Address("Berlin","Pushkina","10"),Date()))
+    list.add(Campaing("BMW", Address("Bawaria","Lenina","101"),Date()))
+    val rsl = getStringDescription(list)
+    println(rsl.get(0))
+    println(rsl.get(1))
+
+}
+
+fun getStringDescription(list: List<Campaing>): List<String> {
+    return list.map({ el -> "Company name : ${el.name} Addres: ${el.address} Date create : ${el.created}"})
+}
+
+data class Campaing(val name: String, val address: Address, val created: Date)
+
+data class Address(val city: String, val street: String, val house: String)
