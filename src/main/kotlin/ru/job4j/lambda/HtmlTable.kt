8e3b325a670be095c7656278abec9640534c4fad
Artyom <tema.ru2011@gmail.com>
2024-02-24 17:26:46 +0300

    EmailService function emailto() use operator with()

--- src/main/kotlin/ru/job4j/lambda/HtmlTable.kt
+++ src/main/kotlin/ru/job4j/lambda/HtmlTable.kt
@@ -19,3 +19,17 @@ class HtmlTable {
         return table.toString()
     }
 }
+
+data class Message(val username: String, val email: String)
+
+class EmailService {
+
+    fun emailTo(message: Message): String {
+        return with(StringBuilder()) {
+            append("Subject : ").append(message.email)
+            append("Body : ").append("Hello, ").append(message.email).append(" ")
+            append("You win!")
+        }.toString()
+
+    }
+}
